cb7cdb84b8c78385e309aadd359aff8dcbbcab48
Jason Tang <jasontang20043@gmail.com>
2026-02-19 19:05:32 -0800

    feat: copy directory

---
+++ src/copystatic.py
@@ -0,0 +1,36 @@
+from os import path, listdir, mkdir
+from shutil import copy, rmtree
+
+'''
+    Utility function to copy directory from source to destination
+    Args:
+        src (str): REQUIRED - Source directory path
+        dst (str): REQUIRED - Destination directory path
+    Returns:
+        None
+
+'''
+def copy_directory(src: str, dest: str):
+    if not path.exists(src):
+        raise ValueError("Source directory does not exist")
+    
+    if path.exists(dest):
+        raise ValueError("Destination directory already exists, should've been removed")
+    
+    # Create destination directory
+    mkdir(dest)
+    print(f"Created destination directory {dest}")
+
+    for direntry in listdir(src):
+        src_entry = path.join(src, direntry)
+        dest_entry = path.join(dest, direntry)
+
+        print(f"Copying {src_entry} to {dest_entry}")
+
+        if path.isfile(src_entry):
+            print(f"{src_entry} is a file, copying...\n")
+            copy(src_entry, dest_entry)
+        else:
+            print(f"{src_entry} is a directory, copying recursively...\n")
+            copy_directory(src_entry, dest_entry)
+    
--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from textnode import *
+from os import path
+from shutil import rmtree
+from copystatic import copy_directory
 
 def main():
-    # Create a new TextNode object with dummy values
-    node = TextNode("Hello World", TextType.PLAIN_TEXT)
-    node_2 = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
+    static_path = path.join(path.dirname(path.dirname(__file__)), "static")
+    public_path = path.join(path.dirname(path.dirname(__file__)), "public")
 
-    print("TextNode objects created:")
-    print(node)
-    print(node_2)
+    if path.exists(public_path):
+        print(f"Public directory {public_path} already exists, deleting it to keep copy clean.")
+        rmtree(public_path)
 
+    copy_directory(static_path, public_path)
+    
 if __name__ == "__main__":
     main()
